faacdb80a2af8993d4443f4fac83e52377e2f1d9
azizullah2017 <31694122+azizullah2017@users.noreply.github.com>
2022-06-27 13:50:53 +0500

    Update es_spark_test.py

--- es_spark_test.py
+++ es_spark_test.py
@@ -1,3 +1,5 @@
+# https://medium.com/@akkidx/indexing-into-elasticsearch-using-spark-code-snippets-55eabc753272
+
 # spark-submit --master local[4] --jars /opt/spark/jars//elasticsearch-hadoop-7.17.4.jar --packages org.elasticsearch:elasticsearch-spark-20_2.11:5.3.1 es_spark_test.py
 
 # https://spark-packages.org/package/elastic/elasticsearch-hadoop
